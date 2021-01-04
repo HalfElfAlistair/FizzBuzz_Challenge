b5b27e43207d3c9cdb3b7c1c62857291398a536b
HalfElfAlistair <ajss90@hotmail.co.uk>
2021-01-04 15:07:20 +0000

    refurbed project with HTML, CSS & JS

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,18 @@
-let num = 1;
-
-for (; num < 101; num++) {
-if ((num % 3 == 0) && (num % 5 == 0)) {
-alert ( 'FizzBuzz' )
-} else if (num % 3 == 0) {
-alert( 'Fizz' );
-} else if (num % 5 == 0) {
-alert( 'Buzz' );
-} else {
-alert( num );
-}
+function fizzbuzzCounter() {
+	let value = parseInt(document.getElementById('count').value, 10);
+	if (value >= 100) {
+        value = 0;
+    }
+	value++;
+	document.getElementById('count').value = value;
+	let result = value;
+	if ((value % 3 == 0) && (value % 5 == 0)) {
+    document.getElementById('fb-result').innerHTML = 'FizzBuzz';
+    } else if (result % 3 == 0) {
+    document.getElementById('fb-result').innerHTML = 'Fizz';
+    } else if (value % 5 == 0) {
+    document.getElementById('fb-result').innerHTML = 'Buzz';
+    } else {
+	document.getElementById('fb-result').innerHTML = result;
+	}
 }
