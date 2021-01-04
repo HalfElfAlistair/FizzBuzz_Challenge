1c7d99d3bce56154a4f1de3ad05cce3f2e7c61dd
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2021-01-04 15:22:14 +0000

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,27 @@
-let num = 1;
+function fizzbuzzCounter() {
+	let value = parseInt(document.getElementById('count').value, 10);
+	if (value >= 100) {
+        value = 0;
+    }
+	value++;
+	document.getElementById('count').value = value;
+	let result = value;
+	if ((value % 3 == 0) && (value % 5 == 0)) {
+    document.getElementById('fb-result').innerHTML = 'FizzBuzz';
+    } else if (result % 3 == 0) {
+    document.getElementById('fb-result').innerHTML = 'Fizz';
+    } else if (value % 5 == 0) {
+    document.getElementById('fb-result').innerHTML = 'Buzz';
+    } else {
+	document.getElementById('fb-result').innerHTML = result;
+	}
+}
+
+
+
+//original code using alert rather than a button//
+
+/* let num = 1;
 
 for (; num < 101; num++) {
 if ((num % 3 == 0) && (num % 5 == 0)) {
@@ -10,4 +33,4 @@ alert( 'Buzz' );
 } else {
 alert( num );
 }
-}
+} */
